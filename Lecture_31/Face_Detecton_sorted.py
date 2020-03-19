a26f7f27ee021bbc83292c0a33d0689ff53bf8b7
Namangarg007 <namangarg679@gmail.com>
2020-03-19 14:45:52 +0530

    CV2

--- Lecture_31/Face_Detecton_sorted.py
+++ Lecture_31/Face_Detecton_sorted.py
@@ -8,24 +8,25 @@ while True:
 
     retval, image = cap.read()
 
-    faces = classifire.detectMultiScale(image)
-    faces = sorted(faces, key=lambda face: face[2]*face[3], reverse=True)
+    if retval:
+        faces = classifire.detectMultiScale(image)
+        faces = sorted(faces, key=lambda face: face[2]*face[3], reverse=True)
 
-    if len(faces) >= 2:
+        if len(faces) >= 2:
 
-        x1, y1, w1, h1 = faces[0]
-        x2, y2, w2, h2 = faces[1]
+            x1, y1, w1, h1 = faces[0]
+            x2, y2, w2, h2 = faces[1]
 
-        face1 = image[y1:y1+h1, x1:x1+w1]
-        face2 = image[y2:y2+h2, x2:x2+w2]
+            face1 = image[y1:y1+h1, x1:x1+w1]
+            face2 = image[y2:y2+h2, x2:x2+w2]
 
-        face1_f = cv2.resize(face2, face1.shape())
-        face2_f = cv2.resize(face1, face2.shape())
+            face1_f = cv2.resize(face2, (w1, h1))
+            face2_f = cv2.resize(face1, (w2, h2))
 
-        face1[:] = face1_f
-        face2[:] = face2_f
+            face1[:] = face1_f
+            face2[:] = face2_f
 
-        cv2.imshow("Swap", image)
+            cv2.imshow("Swap", image)
 
     key = cv2.waitKey(10)
 
